c993e351b114f187e24eb1f3706088b6bd76f706
NISH1001 <nishanpantha@gmail.com>
2019-04-03 20:58:44 +0545

    Add OS-aware path separator
    Use `os.path.join` to join base directory with filenames.

--- postutils.py
+++ postutils.py
@@ -10,24 +10,24 @@ def create_dir(dirname):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
-def save_post_as_text(post, dir="data/"):
-    fname = dir + post['timestamp'] + '-' + re.sub(r'\s+', '-', post['title'])
+def save_post_as_text(post, dir="data"):
+    fname = os.path.join(dir, post['timestamp'] + '-' + re.sub(r'\s+', '-', post['title']))
     print("Filename :: {}".format(fname))
     with open(fname, 'w') as f:
         f.write(textwrap.fill(post['content'], 140))
 
-def save_post_as_json(post, dir="data/"):
-    fname = dir + re.sub(r'\s+', '-', post['title']) + ".json"
+def save_post_as_json(post, dir="data"):
+    fname = os.path.join(dir, re.sub(r'\s+', '-', post['title']) + ".json")
     print("Filename :: {}".format(fname))
     with open(fname, 'w') as f:
         json.dump(post, f, indent=4)
 
-def save_json_single(posts, dir="data/"):
-    fname = "dump.json"
+def save_json_single(posts, dir="data"):
+    fname = os.path.join(dir, "dump.json")
     with open(fname, 'w') as f:
         json.dump(posts, f, indent=4)
 
-def save_posts(posts, dir="data/", dump_type='text'):
+def save_posts(posts, dir="data", dump_type='text'):
     print("Dumping all the shit...")
     print("Total number of posts :: {}".format(len(posts)))
     func = save_post_as_text if dump_type=='text' else save_post_as_json
@@ -36,9 +36,7 @@ def save_posts(posts, dir="data/", dump_type='text'):
 
 
 def main():
-    link = "https://medium.com/@nishparadox/the-sound-of-life-ffb582f060de"
-    post = get_post(link)
-    print(post)
+    pass
 
 if __name__ == "__main__":
     main()
